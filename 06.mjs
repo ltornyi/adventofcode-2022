80aafbc44c2ab6ef3b1adc904db32a8f9abfd791
Lajos Tornyi <ltornyi@gmail.com>
2022-12-10 12:19:12 +0100

    day 6

---
+++ 06.mjs
@@ -0,0 +1,45 @@
+import { equalTest, readInputFile } from "./lib.mjs";
+const input = readInputFile('./06input.txt');
+
+const signal = input[0];
+
+const uniqueChars = (str) => [...new Set(str.split(''))].join('')
+
+const firstUniqueSequenceStart = (signal, len) => {
+  let start = 0;
+  let uniqueStart = -1;
+  while (start < signal.length - len) {
+    const curr = signal.substring(start, start + len);
+    if (uniqueChars(curr).length === len) {
+      uniqueStart = start;
+      break;
+    }
+    start++;
+  }
+  return uniqueStart
+}
+
+const examplesForQ1 = () => {
+  equalTest('mjqjpqmgbljsphdztnvjfqwrcgsmlb', firstUniqueSequenceStart('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 4) + 4, 7);
+  equalTest('bvwbjplbgvbhsrlpgdmjqwftvncz', firstUniqueSequenceStart('bvwbjplbgvbhsrlpgdmjqwftvncz', 4) + 4, 5);
+  equalTest('nppdvjthqldpwncqszvftbrmjlhg', firstUniqueSequenceStart('nppdvjthqldpwncqszvftbrmjlhg', 4) + 4, 6);
+  equalTest('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', firstUniqueSequenceStart('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4) + 4, 10);
+  equalTest('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', firstUniqueSequenceStart('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4) + 4, 11);
+}
+
+const question1 = () => console.log(firstUniqueSequenceStart(signal, 4) + 4)
+
+const examplesForQ2 = () => {
+  equalTest('mjqjpqmgbljsphdztnvjfqwrcgsmlb', firstUniqueSequenceStart('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 14) + 14, 19);
+  equalTest('bvwbjplbgvbhsrlpgdmjqwftvncz', firstUniqueSequenceStart('bvwbjplbgvbhsrlpgdmjqwftvncz', 14) + 14, 23);
+  equalTest('nppdvjthqldpwncqszvftbrmjlhg', firstUniqueSequenceStart('nppdvjthqldpwncqszvftbrmjlhg', 14) + 14, 23);
+  equalTest('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', firstUniqueSequenceStart('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 14) + 14, 29);
+  equalTest('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', firstUniqueSequenceStart('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 14) + 14, 26);
+}
+
+const question2 = () => console.log(firstUniqueSequenceStart(signal, 14) + 14)
+
+examplesForQ1();
+examplesForQ2();
+question1();
+question2();
--- lib.mjs
+++ lib.mjs
@@ -3,3 +3,11 @@ import fs from 'fs';
 export const readInputFile = (filename) => {
   return fs.readFileSync(filename).toString().split("\n");
 }
+
+export const equalTest = (tstMsg, inp1, inp2) => {
+  if (inp1 === inp2) {
+    console.log(tstMsg + ': passed');
+  } else {
+    console.log(tstMsg + `: FAILED, expected ${inp1}, got ${inp2}`);
+  }
+}
